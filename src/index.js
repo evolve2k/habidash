f3969d198c200aa7021b53f0866b6eb458b63e8b
Richie Khoo <evolve2k@gmail.com>
2020-05-09 14:30:05 +0930

    Webpack setup

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ const { VueLoaderPlugin } = require('vue-loader');
 const { HotModuleReplacementPlugin } = require('webpack');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 
+import './style.css';
+
 module.exports = {
   entry: join(__dirname, 'index.js'), 
   output: {
