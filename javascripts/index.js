bcd01d6e7ad8c2577285b5750606965a01f4326d
Richie Khoo <evolve2k@gmail.com>
2020-05-09 18:37:41 +0930

    Axios call to habitica API now works and authenticates.

--- javascripts/index.js
+++ javascripts/index.js
@@ -9,12 +9,26 @@ new Vue({
       token: ''
     }
   },
+  
   mounted () {
+    this.author_uuid_project = '1eb4b652-00a0-467a-b70b-adba9bdb7edf-habidash'
     this.uuid = localStorage.uuid;
     this.token = localStorage.token;
-    axios
-      .get('https://habitica.com/api/v3/status')
-      .then(response => (this.info = response.data.data))
+    const instance = axios.create({
+      baseURL: 'https://habitica.com/api/v3',
+      timeout: 1000,
+      headers: {
+        'Content-Type': 'application/json',
+        'x-api-user': this.uuid,
+        'x-api-key': this.token,
+        'x-client': this.author_uuid_project,
+      }
+    });
+    
+
+    instance
+      .get('/groups?type=party,guilds')
+      .then(response => (this.info = response))
       .catch(error => {
         console.log(error)
         this.errored = true
